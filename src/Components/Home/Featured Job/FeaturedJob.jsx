1e970fa5b8f9be38c580cbe496abacd89453ae0d
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2024-09-09 20:45:02 +0600

    Featured jobs

---
+++ src/Components/Home/Featured Job/FeaturedJob.jsx
@@ -0,0 +1,10 @@
+const FeaturedJob = () => {
+    return (
+        <div className="text-center">
+            <h1 className="text-5xl">Featured Jobs</h1>
+            <p>Explore thousands of job opportunities with all the information you need. Its your future</p>
+        </div>
+    );
+};
+
+export default FeaturedJob;
