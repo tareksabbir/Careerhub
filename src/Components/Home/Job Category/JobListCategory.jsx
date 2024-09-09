5390d13baf2d948ae653a6ab726bd417396b1941
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2024-09-09 20:48:08 +0600

    job list category

---
+++ src/Components/Home/Job Category/JobListCategory.jsx
@@ -0,0 +1,13 @@
+const JobListCategory = () => {
+  return (
+    <div className="text-center">
+      <h1 className="text-5xl">Job Category List</h1>
+      <p>
+        Explore thousands of job opportunities with all the information you
+        need. Its your future
+      </p>
+    </div>
+  );
+};
+
+export default JobListCategory;
