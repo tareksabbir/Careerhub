9eb1d6a1c150a2053556ab60be33ec60a7569dce
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2024-09-14 04:11:33 +0600

    job details page done

--- src/Components/Home/Featured Job/JobDetails.jsx
+++ src/Components/Home/Featured Job/JobDetails.jsx
@@ -1,10 +1,10 @@
-import { useLoaderData, useParams } from "react-router-dom";
+import { Link, useLoaderData, useParams } from "react-router-dom";
 import Background from "../../Background/Background";
-import money from "../../../assets/icons/money.png"
-import calendar from "../../../assets/icons/calendar.png"
-import phone from "../../../assets/icons/phone.png"
-import email from "../../../assets/icons/email.png"
-import address from "../../../assets/icons/Location.png"
+import money from "../../../assets/icons/money.png";
+import calendar from "../../../assets/icons/calendar.png";
+import phone from "../../../assets/icons/phone.png";
+import email from "../../../assets/icons/email.png";
+import address from "../../../assets/icons/Location.png";
 
 const JobDetails = () => {
   const { id } = useParams();
@@ -14,13 +14,12 @@ const JobDetails = () => {
 
   return (
     <>
-     
       <Background title={"Job Description"}></Background>
-     
 
-      <div className="container mx-auto mt-20 mb-32">
-        <div className="grid grid-cols-3 gap-5 p-10">
-          <div className="col-span-2">
+      <div className="container px-10 mx-auto my-40">
+        <div className="grid grid-cols-3 gap-5 ">
+          <div className="col-span-2 ">
+            <img src={data.logo} className="w-[100px] max-h-[50px]" alt="" />
             <h1 className="mt-5">
               <span className="font-bold">Job Description: </span>
               {data.job_description}
@@ -38,35 +37,57 @@ const JobDetails = () => {
               <h1 className="mt-5">{data.experiences}</h1>
             </div>
           </div>
-          <div className="rounded-xl bg-gradient-to-r from-purple-50 to-blue-50">
-            <div className="p-10 ">
-              <h1 className="mb-3 text-lg font-bold"> Job Details</h1>
-              <hr />
-              <div className="flex gap-2 mt-3">
-                <img src={money} alt="" />
-                <h1 ><span className="font-bold">Salary : </span>{data.salary}</h1>
+          <div>
+            <div className="rounded-xl bg-gradient-to-r from-purple-50 to-blue-50">
+              <div className="p-10 ">
+                <h1 className="mb-3 text-lg font-bold"> Job Details</h1>
+                <hr />
+                <div className="flex gap-2 mt-3">
+                  <img src={money} alt="" />
+                  <h1>
+                    <span className="font-bold">Salary : </span>
+                    {data.salary}
+                  </h1>
+                </div>
+                <div className="flex gap-2 mt-2">
+                  <img src={calendar} alt="" />
+                  <h1>
+                    <span className="font-bold">Job Title : </span>
+                    <span>{data.job_title}</span>
+                  </h1>
+                </div>
+                <h1 className="mt-5 mb-3 text-lg font-bold">
+                  Contact Information
+                </h1>
+                <hr />
+                <div className="flex gap-2 mt-3">
+                  <img src={phone} alt="" />
+                  <h1>
+                    <span className="font-bold">Phone: </span>
+                    <span>{data.contact_information.phone}</span>
+                  </h1>
+                </div>
+                <div className="flex gap-2 mt-2">
+                  <img src={email} alt="" />
+                  <h1>
+                    <span className="font-bold">Email : </span>
+                    <span>{data.contact_information.email}</span>
+                  </h1>
+                </div>
+                <div className="flex gap-2 mt-2">
+                  <img src={address} alt="" />
+                  <h1>
+                    <span className="font-bold">Address: </span>
+                  </h1>
+                  <h1>{data.contact_information.address}</h1>
+                </div>
               </div>
-              <div className="flex gap-2 mt-2">
-                <img src={calendar} alt="" />
-                <h1><span className="font-bold">Job Title : </span><span>{data.job_title}</span></h1>
-              </div>
-              <h1 className="mt-5 mb-3 text-lg font-bold">Contact Information</h1>
-              <hr />
-              <div className="flex gap-2 mt-3">
-                <img src={phone} alt="" />
-                <h1><span className="font-bold">Phone: </span><span>{data.contact_information.phone}</span></h1>
-              </div>
-              <div className="flex gap-2 mt-2">
-                <img src={email} alt="" />
-                <h1><span className="font-bold">Email : </span><span>{data.contact_information.email}</span></h1>
-              </div>
-              <div className="flex gap-2 mt-2">
-                <img src={address} alt="" />
-                <h1><span className="font-bold">Address : </span><span>{data.contact_information.address}</span></h1>
-              </div>
-              
-              
             </div>
+           <div  className="w-full py-5 mt-5 font-bold text-center text-white border rounded bg-gradient-to-r from-purple-400 to-blue-400">
+           <Link
+       >Apply Now
+          </Link>
+           </div>
           </div>
         </div>
       </div>
